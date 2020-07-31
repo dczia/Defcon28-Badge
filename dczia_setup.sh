252db27368622855d21cc1fbc45639ccfb45fdf6
Lithochasm <lithochasm@gmail.com>
2020-07-31 02:39:09 -0700

    Sigh...

--- dczia_setup.sh
+++ dczia_setup.sh
@@ -91,6 +91,7 @@ sudo apt-get -y update
 #sudo apt-get upgrade
 sudo apt-get -y install omxplayer cmake vim git chocolate-doom
 git clone https://github.com/juj/fbcp-ili9341.git
+git lfs fetch
 echo ""
 
 ###############################
