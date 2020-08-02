6439b78d6e331f3b77aa91597d18cefbf4317325
LithoChasm <lithochasm@gmail.com>
2020-08-02 11:54:48 -0700

    Hyper fixes

--- hyperpixel_setup.sh
+++ hyperpixel_setup.sh
@@ -117,18 +117,18 @@ echo "$grn Checking for DCZia Boot Settings $white"
 if ! grep -q "DCZia_Hackz" /boot/config.txt; then
         echo "$red Updating /boot/config.txt - Enabling Speed Hacks $white"
         #cat /home/pi/Defcon28-Badge/boot_hacks | sudo tee -a /boot/config.txt > /dev/null
-	sudo sed -i -e '$a gpu_mem=64' /boot/config.txt
-	sudo sed -i -e '$a disable_overscan=1' /boot/config.txt
+	#sudo sed -i -e '$a gpu_mem=64' /boot/config.txt
+	#sudo sed -i -e '$a disable_overscan=1' /boot/config.txt
 	sudo sed -i -e '$a hdmi_force_hotplug=1' /boot/config.txt
 	sudo sed -i -e '$a hdmi_drive=2' /boot/config.txt
 	sudo sed -i -e '$a hdmi_group=1' /boot/config.txt
 	sudo sed -i -e '$a hdmi_mode=16' /boot/config.txt
-	sudo sed -i -e '$a disable_splash=1' /boot/config.txt
+	#sudo sed -i -e '$a disable_splash=1' /boot/config.txt
 	sudo sed -i -e '$a dtoverlay=sdtweak,overclock_50=84' /boot/config.txt
-	sudo sed -i -e '$a boot_delay=0' /boot/config.txt
+	#sudo sed -i -e '$a boot_delay=0' /boot/config.txt
 	sudo sed -i -e '$a force_turbo=1' /boot/config.txt
 	#sudo systemctl disable triggerhappy.service
-	sudo systemctl disable dphys-swapfile.service
+	#sudo systemctl disable dphys-swapfile.service
 	#sudo systemctl disable keyboard-setup.service
 	#sudo systemctl disable apt-daily.service
 	#sudo systemctl disable wifi-country.service
